fd567f07a93600eaf9ed7c4a6313172248b846cd
joansaldanha21 <70396182+joansaldanha21@users.noreply.github.com>
2021-01-12 22:40:46 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, app
 
 
-global userconvo
-userconvo = []
+
 
 # --- User Interface ---
 
@@ -10,21 +9,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    global userID,userconvo
+    global userID, userconvo
+    userconvo = []
     userID = 123
     intro=[]
     intro.append("Bot : Hi, I am AssistBot. Your customer service agent. How may I help you?")
     mike_status="no"
     resp="Hi, I am AssistBot. Your customer service agent. How may I help you?"
-    userconvo.append("Bot : Hi, I am AssistBot. Your customer service agent. How may I help you?")
+    userconvo.append(str("Bot : Hi, I am AssistBot. Your customer service agent. How may I help you?"))
     killSession="no"
-    return render_template('index.html', user_input=intro,mike_status=mike_status,botResp=resp,userID=userID,killSession=killSession)
+    return render_template('index.html', user_input=intro,mike_status=mike_status,botResp=resp,userID=userID,killSession=killSession,userconvo=userconvo)
 
 @app.route('/process', methods=['POST'])
 def process():  ##called when user input is given and submit button is pressed
-    global userID, userconvo
+    global userID
     print("Process Called")
+    userconvoprev = request.form["userconvo"]
     user_input = request.form["user_input"]
+    print("userconvoprev :",userconvoprev)
+    #userconvo.append(userconvoprev)
     userconvo.append("You : " + user_input)
     if (user_input == "TimeOut"):
        resp="Goodbye"
@@ -40,7 +43,8 @@ def process():  ##called when user input is given and submit button is pressed
     killSession = request.form["killSession"]
     print("killSession : ",killSession)
     rlist=userconvo
-    return render_template("index.html", user_input=rlist,mike_status=mike_status,botResp=resp,userID=userID,killSession=killSession)
+    print ("rlist",rlist)
+    return render_template("index.html", user_input=rlist,mike_status=mike_status,botResp=resp,userID=userID,killSession=killSession,userconvo=userconvo)
 
 #---- Kicking of the main program ----
 
